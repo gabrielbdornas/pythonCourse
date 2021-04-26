98337d769411efded63194b1d6705617cd8ff181
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-04-26 09:30:45 -0300

    ate for loops

---
+++ classes/12-for-loops/for_loops.py
@@ -0,0 +1,33 @@
+# https://www.youtube.com/watch?v=_uQrJ0TkZlc&t=215s
+
+# for loops
+print("Iterando sobre as letras da linguagem de programação que estamos aprendendo")
+for letra in "Python":
+    print(letra)
+print("Fim da iterando sobre as letras da linguagem de programação que estamos aprendendo")
+
+print("Iterando sobre minha lista de compras")
+for item in ['Banana', 'Maçã', 'Abacaxi']:
+    print(item)
+print("Fim da iteração sobre minha lista de compras")
+
+print("Iterando sobre uma lista de números utilizando a função range")
+for item in range(10):
+    print(item)
+print("Fim da iteração sobre uma lista de números utilizando a função range")
+
+# Exercícios:
+print("----------------Exercícios----------------")
+# print todas as letras do seu primeiro nome (cada letra em uma linha)
+
+# crie uma lista simples (até 4 elementos) e print cada um de seus elementos em uma linha
+
+# crie um for loop utilizando a função range com dois critérios
+
+# crie um for loop utilizando a função range com três critérios
+
+# supondo que vc tenha a seguinte lista de preços [10, 15, 27]
+# crie um foor loop para calcular a soma destes itens
+# print o valor final da lista ao final do programa
+
+print("----------------Exercícios----------------")
---
+++ classes/12-for-loops/for_loops_exercices_corrections.py
@@ -0,0 +1,24 @@
+# Exercícios:
+print("----------------Exercícios----------------")
+# print todas as letras do seu primeiro nome (cada letra em uma linha)
+for letra in "Gabriel":
+    print(letra)
+# crie uma lista simples (até 4 elementos) e print cada um de seus elementos em uma linha
+for item in ['Carro', 'Bicicleta', 'Moto']:
+    print(item)
+# crie um for loop utilizando a função range com dois critérios
+for item in range(5, 11):
+    print(item)
+# crie um for loop utilizando a função range com três critérios
+for item in range(5, 16, 5):
+    print(item)
+# supondo que vc tenha a seguinte lista de preços [10, 15, 27]
+# crie um foor loop para calcular a soma destes itens
+# print o valor final da lista ao final do programa
+precos = [10, 15, 27]
+total = 0
+for preco in precos:
+    total += preco
+print(f'O valor total da lista de compras é {total}')
+# crie um foor loop para calcular a soma destes itens
+print("----------------Exercícios----------------")
