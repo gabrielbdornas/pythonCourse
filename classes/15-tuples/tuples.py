7b88dcd888ae1ba513668fa7b1e4a94555332829
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-04-28 20:40:32 -0300

    tuple

---
+++ classes/15-tuples/tuples.py
@@ -0,0 +1,19 @@
+# https://www.youtube.com/watch?v=_uQrJ0TkZlc&t=215s
+
+# tuples
+# não é possível modificar este tipo de "lista"
+# métodos disponíveis apenas index and count
+# count - https://www.w3schools.com/python/ref_tuple_count.asp
+tuple_exemple = (1, 2, 3)
+# não é possivel mudar ou criar um item na lista como tuple_exemple[0] = 10
+print(tuple_exemple[0])
+print(tuple_exemple[1])
+print(tuple_exemple[2])
+print(tuple_exemple.count(1)) # número de vezes que o elemento 1 aparece
+
+# Exercícios:
+print("----------------Exercícios----------------")
+# utilize a função count para printar na tela quantas vezes o primeiro elemento da tuple abaixo aparece na mesma
+numbers = (1, 2, 1, 3, 4, 5, 1)
+
+print("----------------Exercícios----------------")
---
+++ classes/15-tuples/tuples_exercices_corrections.py
@@ -0,0 +1,7 @@
+# Exercícios:
+print("----------------Exercícios----------------")
+# utilize a função count para printar na tela quantas vezes o primeiro elemento da tuple abaixo aparece na mesma
+numbers = (1, 2, 1, 3, 4, 5, 1)
+print(numbers.count(numbers[0]))
+
+print("----------------Exercícios----------------")
