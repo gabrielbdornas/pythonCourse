353af30f1e3e9af787f1976ccb9913a59e7800db
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-05-11 08:39:09 -0300

    random python module

---
+++ classes/22-random-module/random_module.py
@@ -0,0 +1,17 @@
+import random
+
+for i in range(3):
+    print(f"Números randomicos entre 0 e um: {random.random()}")
+
+for i in range(3):
+    print(f"Números randomicos entre 10 e 20: {random.randint(10, 20)}")
+
+time = ['Gabriel', 'Renata', 'Rafael', 'Julio']
+print(random.choice(time))
+
+# Exercícios:
+print("----------------Exercícios----------------")
+# Defina uma classe que contenha uma função que retorne uma lista com dois números gerados randomicamente
+# class ListaRandomica
+# def geracao_lista
+print("----------------Exercícios----------------")
--- classes/22-random-module/random_module_exercices_corrections.py
+++ classes/22-random-module/random_module_exercices_corrections.py
@@ -1,17 +1,19 @@
 import random
-
-for i in range(3):
-    print(f"Números randomicos entre 0 e um: {random.random()}")
-
-for i in range(3):
-    print(f"Números randomicos entre 10 e 20: {random.randint(10, 20)}")
-
-time = ['Gabriel', 'Renata', 'Rafael', 'Julio']
-print(random.choice(time))
-
 # Exercícios:
 print("----------------Exercícios----------------")
 # Defina uma classe que contenha uma função que retorne uma lista com dois números gerados randomicamente
 # class lista_randomica
 # def geracao_lista
+
+
+class ListaRandomica():
+    def geracao_lista(self):
+        first = random.randint(10, 20)
+        second = random.randint(10, 20)
+        return first, second
+
+
+lista_teste = ListaRandomica()
+print(lista_teste.geracao_lista())
+
 print("----------------Exercícios----------------")
